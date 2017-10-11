888b7a515b265539c47a2aac2b14f8fcd079079e
vadimivanenko <vadimivanenko@172.20.10.3>
2017-10-11 10:43:39 +0300

    pr1

---
+++ src/gsu/by/Music/Music.java
@@ -0,0 +1,41 @@
+package gsu.by.Music;
+
+public class Music {
+
+	private String name;
+	private int kol_copy;
+	private Boolean mp3;
+	
+	public Music(String name, int kol_copy, Boolean mp3) {
+		super();
+		this.name = name;
+		this.kol_copy = kol_copy;
+		this.mp3 = mp3;
+	}
+
+	public Music() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKol_copy() {
+		return kol_copy;
+	}
+	public void setKol_copy(int kol_copy) {
+		this.kol_copy = kol_copy;
+	}
+	public Boolean getMp3() {
+		return mp3;
+	}
+	public void setMp3(Boolean mp3) {
+		this.mp3 = mp3;
+	}
+	
+	
+
+}
